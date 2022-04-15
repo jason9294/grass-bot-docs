df24ebe3f3eea0977f9e0bd81050d1aeb074f533
jason9294 <63494593+jason9294@users.noreply.github.com>
2022-04-15 10:37:49 +0800

    .

--- dev/docs/.vuepress/config.js
+++ dev/docs/.vuepress/config.js
@@ -7,17 +7,48 @@ module.exports = {
     },
     themeConfig: {
         smoothScroll: true,
-        sidebar: [
-            '/',
-            {
-              title: 'Daily',
-              path: '/daily/',
-              collapsable: false,
-              children: [
-                '/daily/1A2B'
-              ]
-            }
-        ],
+        nextLinks: false,
+        prevLinks: false,
+        sidebar: {
+            '/guide/': [
+                '',
+              {
+                title: '每日功能',
+                path: '/guide/daily/',      // 可选的
+                collapsable: false, // 可选的, 默认值是 true,
+                sidebarDepth: 1,    // 可选的, 默认值是 1
+                children: [
+                  '/guide/daily/1A2B',
+                  '/guide/daily/covid-19',
+                ]
+              }
+            ],
+            '/daily/1A2B': [
+                {
+                  title: '每日功能',
+                  //path: '/',      // 可选的
+                  collapsable: false, // 可选的, 默认值是 true,
+                  sidebarDepth: 1,    // 可选的, 默认值是 1
+                  children: [
+                    '/daily/1A2B',
+                  ]
+                },
+                {
+                  title: '自訂回覆',
+                  //path: '/',      // 可选的
+                  collapsable: false, // 可选的, 默认值是 true,
+                  sidebarDepth: 1,    // 可选的, 默认值是 1
+                  children: [
+                    '/custom_reply/dictionary',
+                  ]
+                }
+              ],
+
+            // fallback
+            '/': [
+              '',        /* / */
+            ]
+          },
 
         nav: [
             { text: 'Home', link: '/' }
--- dev/docs/readme.md
+++ dev/docs/readme.md
@@ -1,18 +1,15 @@
 ---
 home: true
 heroImage: logo.png
-heroText: Hero 标题
-tagline: Hero 副标题
-actionText: 快速上手 →
-actionLink: /zh/guide/
+heroText: Discord Grass bot
+tagline: By 雜草
+actionText: 開始 →
+actionLink: /guide/
 features:
-- title: 简洁至上
-  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
-- title: Vue驱动
-  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
-- title: 高性能
-  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
-footer: MIT Licensed | Copyright © 2018-present Evan You
+- title: 社群功能
+  details: 使社群活絡的應用指令，增加成員間的互動。
+- title: 實用指令
+  details: 具有許多實用指令，如隨機選擇器、食物產生器等，幫助選擇困難的你。
+- title: 自訂義系統
+  details: 能自訂關鍵字回覆、定時公告、建立伺服器資料庫等等。
 ---
-
-# Home
