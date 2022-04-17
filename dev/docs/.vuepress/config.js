7feab179675d27284822493caf7beec15c3f8d34
jason9294 <63494593+jason9294@users.noreply.github.com>
2022-04-18 04:01:47 +0800

    .

--- dev/docs/.vuepress/config.js
+++ dev/docs/.vuepress/config.js
@@ -2,9 +2,13 @@ module.exports = {
     title: 'Discord grass bot',
     description: 'grass bot',
     base: '/my_first_vuepress/',
+
+    plugins: ['@vuepress/back-to-top'],
+
     markdown: {
         lineNumbers: true
     },
+    
     themeConfig: {
         smoothScroll: true,
         nextLinks: false,
@@ -16,7 +20,7 @@ module.exports = {
                 title: '每日功能',
                 //path: '/guide/daily/',      // 可选的
                 collapsable: false, // 可选的, 默认值是 true,
-                sidebarDepth: 0,    // 可选的, 默认值是 1
+                sidebarDepth: 1,    // 可选的, 默认值是 1
                 children: [
                     '/guide/daily/1A2B',
                     '/guide/daily/covid-19',
